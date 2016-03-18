bdd418be0e01afb3cc15d72aa25ce660bc7f62ab
melgomez <meldarrelgomez@gmail.com>
2016-03-18 14:22:27 +0800

    Add update or edit task function.

--- TaskManager/AddTaskViewController.swift
+++ TaskManager/AddTaskViewController.swift
@@ -14,21 +14,29 @@ class AddTaskViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var detailsTextView: UITextView!
     @IBOutlet weak var titleTextField: UITextField!
     @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var addButton: UIButton!
+    
+    var task: Task = Task()
     
     @IBAction func saveTask(sender: AnyObject) {
+        
+        
         let realm = try! Realm()
-        let task = Task()
-        
-        task.title = titleTextField.text!
-        task.details = detailsTextView.text!
-        task.date = datePicker.date
-        
-        try! realm.write() {
-            realm.add(task)
+        if task.title != "" {
+            try! realm.write {
+                task.setValue(titleTextField.text!, forKeyPath: "title")
+                task.setValue(detailsTextView.text!, forKeyPath: "details")
+                task.setValue(datePicker.date, forKeyPath: "date")
+            }
+        } else {
+            task.title = titleTextField.text!
+            task.details = detailsTextView.text!
+            task.date = datePicker.date
+            
+            try! realm.write() {
+                realm.add(task)
+            }
         }
-        
-//        self.navigationController?.popToRootViewControllerAnimated(true)
-        
     }
     
     override func viewDidLoad() {
@@ -37,6 +45,18 @@ class AddTaskViewController: UIViewController, UITextFieldDelegate {
         self.detailsTextView.layer.borderColor = UIColor(red: 0.9, green: 0.9, blue: 0.9, alpha: 1.0).CGColor
         self.detailsTextView.layer.borderWidth = 1.0
         self.detailsTextView.layer.cornerRadius = 5
+        
+//        print(task.title, task.details, task.date)
+        
+        if task.title != "" {
+            titleTextField.text = task.title
+            detailsTextView.text = task.details
+            datePicker.date = task.date
+            
+            addButton.setTitle("UPDATE TASK", forState: .Normal)
+        } else {
+            addButton.setTitle("ADD NEW TASK", forState: .Normal)
+        }
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
